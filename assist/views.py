6c398c08e9075d1f19e8ebe4685d08d210eee64f
Rajan Chauhan <Rajan Chauhan>
2017-02-24 01:49:06 +0530

    views.home function ready

--- assist/views.py
+++ assist/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response, redirect
 
 # Create your views here.
+def home(request):
+    return render_to_response("index.html",context={'message':"It is good going"})
