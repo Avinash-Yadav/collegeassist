79dc1c2a4832e6b416c63740c6cdccc3dfe8d616
Rajan Chauhan <Rajan Chauhan>
2017-02-24 02:37:26 +0530

    base.html file addition

--- assist/views.py
+++ assist/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render, render_to_response, redirect
 
 # Create your views here.
 def home(request):
+    print("hello")
     return render_to_response("index.html",context={'message':"It is good going"})
+def base(request):
+    print("hello")
+    return render_to_response("index.html",context={'message':"Nothing matters"})
+
--- collegeassist/urls.py
+++ collegeassist/urls.py
@@ -17,5 +17,5 @@ from django.conf.urls import url,include
 from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    # url(r'^/',include('assist.urls')),
+    url(r'^',include('assist.urls')),
 ]
