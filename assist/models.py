55fb7ada2d7fec7642986aa937b8bee66e147614
rajexp <rajanchauhan11@gmail.com>
2017-02-24 10:16:46 +0530

    assist/models.py models created for Dept and Courses

--- assist/models.py
+++ assist/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Department(models.Model):
+    name    = models.CharField(max_length = 50,blank=False,null=False)
+    acronym = models.CharField(max_length=10,blank=False,null=False)
+
+class Course(models.Model):
+    name    = models.CharField(max_length=50,blank=False,null=False)
+    dept    = models.ForeignKey('Department')
+    
