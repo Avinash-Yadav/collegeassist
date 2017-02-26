317eb67903d4c9d959f6ec6267d0f3b3545e05fa
aviyadav294 <aviyadav294@gmail.com>
2017-02-26 18:17:32 +0530

    New models created and forms made

--- assist/admin.py
+++ assist/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Course,Department,User,Student,ExamPapers,Material,Announcements
+from .models import Course,Department,User,Student,ExamPaper,Material,Announcement
 admin.site.empty_value_display = '(None)'
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
@@ -26,17 +26,17 @@ class StudentAdmin(admin.ModelAdmin):
     list_display_links = ('registration_no',)
 admin.site.register(Student,StudentAdmin)
 
-class AnnouncementsAdmin(admin.ModelAdmin):
+class AnnouncementAdmin(admin.ModelAdmin):
     list_display = ('course','description','updated_on','added_on','files','title','author' )
     list_display_links = ('course','title')
-admin.site.register(Announcements,AnnouncementsAdmin)
+admin.site.register(Announcement,AnnouncementAdmin)
 
 class MaterialAdmin(admin.ModelAdmin):
-    list_display = ('course','added_on','files','author' )
+    list_display = ('course','title','added_on','files','author' )
     list_display_links = ('course',)
 admin.site.register(Material,MaterialAdmin)
 
 class ExamPapersAdmin(admin.ModelAdmin):
     list_display = ('course','files','term','author' )
     list_display_links = ('course',)
-admin.site.register(ExamPapers,ExamPapersAdmin)
+admin.site.register(ExamPaper,ExamPapersAdmin)
