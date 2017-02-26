d95607c1fb2156ddde37acdf51842a706af98e89
rajexp <rajanchauhan11@gmail.com>
2017-02-26 18:58:46 +0530

    views.py merged

--- assist/views.py
+++ assist/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, render_to_response, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login , logout
-from .models import Course,Department,User,Student,ExamPapers,Material,Announcements
+from .models import Course, Department, User, Student, ExamPaper, Material, Announcement
+from .forms import RegisterForm , LoginForm , AnnouncementForm , MaterialForm , ExamPaperForm
+from django.contrib.auth.decorators import login_required
 from .forms import RegisterForm , LoginForm
 from django.core import serializers
-from django.contrib.auth.decorators import login_required
 import json 
 from django.http import JsonResponse,HttpResponse
 # Create your views here.
@@ -60,5 +61,61 @@ def getCourses(request,department=None):
         data = json.loads(course)
         result = {"result":data}
         return HttpResponse(json.dumps(result),content_type='application/json')
-        
+
+@login_required
+def Announcements(request,coursecode=None):
+    if request.method =='GET':
+        form= AnnouncementForm()
+        return render(request,"announcements.html",context={"form":form})
+    elif request.method =="POST":
+        form = AnnouncementForm(request.POST,request.FILES)
+        if(form.is_valid()):
+            obj=Announcement()
+            obj.files = form.cleaned_data["files"]
+            obj.title = form.cleaned_data["title"]
+            obj.description = form.cleaned_data["description"]
+            obj.author=request.user
+            obj.course= Course.objects.get(code=coursecode)
+            obj.save()
+            return redirect("home")
+        print(form.errors) 
+        return redirect("home") 
+
+@login_required
+def Materials(request,coursecode=None):
+    if request.method =='GET':
+        form= MaterialForm()
+        return render(request,"material.html",context={"form":form})
+    elif request.method =="POST":
+        form = MaterialForm(request.POST,request.FILES)
+        if(form.is_valid()):
+            obj = Material()
+            obj.files = form.cleaned_data["files"]
+            obj.title = form.cleaned_data["title"]
+            obj.author=request.user
+            obj.course= Course.objects.get(code=coursecode)
+            obj.save()
+            return redirect("home")
+        print(form.errors)
+        return redirect("home") 
+
+@login_required
+def ExamPapers(request,coursecode=None):
+    if request.method =='GET':
+        form= ExamPaperForm()
+        return render(request,"ExamPaper.html",context={"form":form})
+    elif request.method =="POST":
+        form = ExamPaperForm(request.POST,request.FILES)
+        if(form.is_valid()):
+            obj = ExamPaper()
+            obj.files = form.cleaned_data["files"]
+            obj.term = form.cleaned_data["term"]
+            obj.author=request.user
+            obj.course= Course.objects.get(code=coursecode)
+            obj.save()
+            return redirect("home")
+        print(form.errors) 
+        return redirect("home")
+
+
 
